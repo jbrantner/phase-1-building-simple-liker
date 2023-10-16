6299012e4c9e932a6913de55d5ea705ee1e8d413
Jordan Brantner <thejordanbrantner@gmail.com>
2023-10-16 17:42:32 -0400

    WIP

--- main.js
+++ main.js
@@ -2,7 +2,35 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+
+// Your JavaScript code goes here
+// Hides ERROR message
+const error = document.getElementById('modal');
+const hide = () => {error.className = "hidden"};
+const callServer = serverCall(mimicServerCall)
+
+
+function setHidden() {
+  return setTimeout(hide, 3000);
+}
+
+
+
+
+
+// fetch server
+function serverCall(url) {
+  fetch(url)
+  .then(() => console.log('did this work?'))
+  .then((response) => response.json())
+  .catch((err) => {
+    const text = document.getElementById("modal").querySelector('h2');
+    text.textContent = err
+    const modalDiv = document.getElementById('modal');
+    modalDiv.removeAttribute('hidden')
+    setHidden();
+  })
+}
 
 
 
